2f02b3c2db5c7bd157dcd7d0ad8a9d5fb31d5a58
Irina <iirinau@mail.ru>
2022-12-18 22:25:57 +0500

    Семинар 1. Задача 4. ДЗ

---
+++ Seminar_1_Task4/Program.cs
@@ -0,0 +1,25 @@
+﻿// Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+// 2, 3, 7 -> 7
+// 44 5 78 -> 78
+// 22 3 9 -> 22
+
+Console.Write("Введите целое число A: ");
+int numA = int.Parse(Console.ReadLine());
+
+Console.Write("Введите целое число B: ");
+int numB = int.Parse(Console.ReadLine());
+
+Console.Write("Введите целое число C: ");
+int numC = int.Parse(Console.ReadLine());
+
+int max = numA;
+
+if (max < numB)
+{
+    max = numB;
+    if (max < numC)
+    {
+        max = numC;
+    }
+}
+Console.WriteLine("max = " + max);

df62319a059f8ee2c9c45801856c3de8c44f4fca
Irina <iirinau@mail.ru>
2022-12-20 12:30:21 +0500

    Семинар 3. Задача 19. ДЗ

---
+++ Seminar_3_Task19/Program.cs
@@ -0,0 +1,24 @@
+﻿// Задача 19
+// Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+// 14212 -> нет
+// 12821 -> да
+// 23432 -> да
+
+Console.Write("Введите целое ПЯТИзначное число: ");
+int value = int.Parse(Console.ReadLine());
+
+// Выделяем разряды введенного числа
+int digit5 = value / 10000;
+int digit4 = (value / 1000) % 10;
+int digit3 = (value / 100) % 10;
+int digit2 = (value / 10) % 10;
+int digit1 = value % 10;
+
+if ((digit5 == digit1) && (digit4 == digit2))
+{
+    Console.WriteLine($"Число {value} является полиндромом");
+}
+else
+{
+    Console.WriteLine($"Число {value} НЕ является полиндромом");
+}
